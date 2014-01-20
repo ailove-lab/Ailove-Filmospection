2a71962c23926fb8fc36a61278e615d084727f4d
peko <peko@gasubasu.com>
2014-01-20 20:53:31 +0400

    first commit

---
+++ src/filmospector.c
@@ -0,0 +1,12 @@
+/*
+	film duration ~ 90 min = 5400 sec = 162000 frames
+	162000 frames - can be backed in quad 400x400 +
+	closest power of 2 - 512x512 quad (zoom 9, ~145min)
+*/
+
+#include "options.h"
+int main(int argc, char** argv) {
+
+	parseOptions(argc, argv)
+
+}
